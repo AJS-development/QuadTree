0a80b3922c915a58e8838a7387fcf544b4ef2b52
Andrew54757 <andrews54757@gmail.com>
2016-09-21 09:43:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+var QTree = class QTree{
+  
+  constructor(bounds,maxobj,maxlevl,level,parent) {
+  this.bounds = bounds;
+  this.maxobj = maxobj
+  this.level = level
+  this.parent = parent;
+  }
+  
+  
+}
